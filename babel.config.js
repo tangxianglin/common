0e613c78bf4e03d45b265aff3e436872798894f3
t_xianglin <t_xianglin@163.com>
2019-10-10 16:43:20 +0800

    initial commit

---
+++ babel.config.js
@@ -0,0 +1,18 @@
+const presets = [
+  [
+    "@babel/env",
+    {
+      targets: {
+        edge: "17",
+        firefox: "60",
+        chrome: "67",
+        safari: "11.1",
+        ie: '11',
+      },
+      useBuiltIns: "usage", // "usage" | "entry" | false, defaults to false.
+      corejs: "3.2.1",
+    },
+  ],
+];
+
+module.exports = { presets };
